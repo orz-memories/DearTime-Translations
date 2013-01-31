6616eddd55b773633f170d8f0bde10f710a1b164
Yeechan Lu <wz.bluesnow@gmail.com>
2013-01-31 19:02:15 +0800

    Update sync.rb

--- sync.rb
+++ sync.rb
@@ -1,53 +1,57 @@
 #!/usr/bin/env ruby
 #encoding: utf-8
-def log m
-  [STDERR].each do |o|
-    o.puts m
-  end
-end
-
-def loadphp file
-  content = File.open file, "r" do |io|
-    io.read
-  end
-  result = {}
-  content.scan(/^\s*(['"])([A-Za-z0-9_]*?)\1\s*=>\s*(['"])(.*?)\3,?\s*$/).each do |i|
-    result[i[1]] = i[2] + i[3] + i[2]
-  end
-  result
-end
-
-def dumpphp file, list
-  result = ""
-  result << "<?php" << "\n"
-  result << "return array(" << "\n"
-  list.keys.sort.each do |k|
-    result << "\t'" << k << "' => " << list[k] << ",\n"
-  end
-  result << ");"
-  File.open file, "w" do |io|
-    io.write result
-  end
-end
-
-log "Bot: rewriting"
-
-ORIGIN = "zh-cn.php"
-origin = loadphp ORIGIN
-
-Dir["*.php"].each do |file|
-  list = loadphp file
-  result = {}
-  (list.keys - origin.keys).each do |k|
-    log "invalid key in #{file}: \"#{k}\" => #{list[k]}"
-  end
-  origin.keys.each do |k|
-    if list[k]
-      result[k] = list[k]
-    else
-      log "not found in #{file}: \"#{k}\". using origin."
-      result[k] = origin[k]
+begin
+  def log m
+    [STDERR].each do |o|
+      o.puts m
     end
   end
-  dumpphp file, result
+  
+  def loadphp file
+    content = File.open file, "r" do |io|
+      io.read
+    end
+    result = {}
+    content.scan(/^\s*(['"])([A-Za-z0-9_]*?)\1\s*=>\s*(['"])(.*?)\3,?\s*$/).each do |i|
+      result[i[1]] = i[2] + i[3] + i[2]
+    end
+    result
+  end
+  
+  def dumpphp file, list
+    result = ""
+    result << "<?php" << "\n"
+    result << "return array(" << "\n"
+    list.keys.sort.each do |k|
+      result << "\t'" << k << "' => " << list[k] << ",\n"
+    end
+    result << ");"
+    File.open file, "w" do |io|
+      io.write result
+    end
+  end
+  
+  log "Bot: rewriting"
+  
+  ORIGIN = "zh-cn.php"
+  origin = loadphp ORIGIN
+  
+  Dir["*.php"].each do |file|
+    list = loadphp file
+    result = {}
+    (list.keys - origin.keys).each do |k|
+      log "invalid key in #{file}: \"#{k}\" => #{list[k]}"
+    end
+    origin.keys.each do |k|
+      if list[k]
+        result[k] = list[k]
+      else
+        log "not found in #{file}: \"#{k}\". using origin."
+        result[k] = origin[k]
+      end
+    end
+    dumpphp file, result
+  end
+rescue
+  puts $!
 end
